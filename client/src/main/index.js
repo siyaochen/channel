f7a122b69cb5a5e43f16e81ad000e9bd92d7bc7e
Siyao Chen <siyaochen416@gmail.com>
2020-10-27 20:56:58 -0400

    Clean up files

--- client/src/main/index.js
+++ client/src/main/index.js
@@ -1,15 +1,11 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
-import { NavBar } from '../components'
-import { MessagesList, MessagesCreate } from '../pages'
-
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-function App() {
+const App = () => {
     return (
         <Router>
-            <NavBar />
             <Switch>
                 <Route path="/message/create" exact component={MessagesCreate} />
                 <Route path="/message/list" exact component={MessagesList} />
--- client/src/pages/index.js
+++
@@ -1,4 +0,0 @@
-import MessagesList from './MessagesList'
-import MessagesCreate from './MessagesCreate'
-
-export { MessagesList, MessagesCreate }
